4d566774e24749318a3e5efc788536e50cce1d2d
Gixxy211 <119839507+Gixxy211@users.noreply.github.com>
2026-02-11 19:05:02 -0500

    Update config.lua

--- qbx_harness/shared/config.lua
+++ qbx_harness/shared/config.lua
@@ -1,12 +1,14 @@
-return {
-    -- Basic settings
-    Framework = 'qbx',
-    Inventory = 'ox',
-    Notify = 'ox',
-    
-    -- Progress bar type
-    Progress = 'ox_bar',
-    
-    -- Debug mode
-    Debug = false
-}
+Config = {}
+
+-- If you use the np fake plate system, set this to true
+Config.npFakePlate = false
+
+-- Time in milliseconds to install the harness (Progress Bar)
+Config.InstallTime = 10000
+
+-- Harness item name (ox_inventory item name)
+Config.Harness = 'harness'
+
+-- If enabled, using a harness in a vehicle that already has one
+-- will uninstall the existing harness
+Config.UninstallHarnessWithItem = true
